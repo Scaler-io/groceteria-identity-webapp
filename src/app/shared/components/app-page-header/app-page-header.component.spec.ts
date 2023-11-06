ecdc7e3b7fd83674d41bacbcba7f25087407114e
sharthak95 <sharthakmallik@gmail.com>
2023-11-07 00:06:20 +0530

    unit test fix

--- src/app/shared/components/app-page-header/app-page-header.component.spec.ts
+++ src/app/shared/components/app-page-header/app-page-header.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AppPageHeaderComponent } from './app-page-header.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AppPageHeaderComponent', () => {
   let component: AppPageHeaderComponent;
@@ -8,7 +9,8 @@ describe('AppPageHeaderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AppPageHeaderComponent ]
+      declarations: [ AppPageHeaderComponent ],
+      imports: [RouterTestingModule]
     })
     .compileComponents();
   });
